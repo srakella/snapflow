7ff3583f210688793641c64cc5f9c77c4a41eaf2
srakella <srakella19@gmail.com>
2026-01-17 08:31:56 -0500

    feat: initial project structure for SnapFlow

---
+++ snapflow-engine/src/main/java/com/snapflow/engine/SnapFlowEngineApplication.java
@@ -0,0 +1,13 @@
+package com.snapflow.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapFlowEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapFlowEngineApplication.class, args);
+    }
+
+}
